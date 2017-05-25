a66471d58c96de5cce23aaa80c1ddebf0ab393aa
Gabriel Palhares <gaapalhares@gmail.com>
2017-05-25 09:39:22 -0300

    teste

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,6 +54,6 @@ module.exports = function(grunt) {
     }
   });
  
-  grunt.registerTask('default', ['mochaTest', 'jshint', 'uglify',]);
+  grunt.registerTask('default', ['mochaTest', 'jshint', 'uglify', 'circleci']);
  
 };
